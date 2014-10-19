1c99d209b5e882f5c57a90436621d66fc142d55d
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2014-10-19 07:38:28 +0200

    corrected router names

--- index.php
+++ index.php
@@ -31,42 +31,43 @@
                 <option value="gluon-fffl-<?php echo $versionNumber; ?>-d-link-dir-615-rev-e1">D-Link 615</option>
                 <option value="gluon-fffl-<?php echo $versionNumber; ?>-d-link-dir-825-rev-b1">D-Link 825</option>
                 <option value="gluon-fffl-<?php echo $versionNumber; ?>-linksys-wrt160nl">Linksys WRT 160 NL</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3020-v1">Tp-Link TL MR 3020 v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3040-v1">Tp-Link TL MR 3040 v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3220-v1">Tp-Link TL MR 3220 v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3420-v1">Tp-Link TL MR 3420 v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3420-v2">Tp-Link TL MR 3420 v2</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wa901n-nd-v2">Tp-Link TL WA 901n-nd v2</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wdr3500-v1">Tp-Link TL WDR 3500 v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wdr3600-v1">Tp-Link TL WDR 3600 v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wdr4300-v1">Tp-Link TL WDR 4300 v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr740n-nd-v1">Tp-Link TL WR 740n-nd v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr740n-nd-v3">Tp-Link TL WR 740n-nd v3</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr740n-nd-v4">Tp-Link TL WR 740n-nd v4</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr741n-nd-v1">Tp-Link TL WR 741n-nd v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr741n-nd-v2">Tp-Link TL WR 741n-nd v2</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr741n-nd-v4">Tp-Link TL WR 741n-nd v4</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v3">Tp-Link TL WR 841n-nd v3</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v5">Tp-Link TL WR 841n-nd v5</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v7">Tp-Link TL WR 841n-nd v7</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v8">Tp-Link TL WR 841n-nd v8</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v9">Tp-Link TL WR 841n-nd v9</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr842n-nd-v1">Tp-Link TL WR 842n-nd v1</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr842n-nd-v2">Tp-Link TL WR 842n-nd v2</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr941n-nd-v2">Tp-Link TL WR 941n-nd v2</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr941n-nd-v3">Tp-Link TL WR 941n-nd v3</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr941n-nd-v4">Tp-Link TL WR 941n-nd v4</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr1043n-nd-v1">Tp-Link TL WR 1043n-nd v1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3020-v1">TP-Link TL MR 3020 Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3040-v1">TP-Link TL MR 3040 Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3220-v1">TP-Link TL MR 3220 Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3420-v1">TP-Link TL MR 3420 Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-mr3420-v2">TP-Link TL MR 3420 Ver:2</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wa901n-nd-v2">TP-Link TL WA 901 N/ND Ver:2</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wdr3500-v1">TP-Link TL WDR 3500 Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wdr3600-v1">TP-Link TL WDR 3600 Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wdr4300-v1">TP-Link TL WDR 4300 Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr740n-nd-v1">TP-Link TL WR 740 N/ND Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr740n-nd-v3">TP-Link TL WR 740 N/ND Ver:3</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr740n-nd-v4">TP-Link TL WR 740 N/ND Ver:4</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr741n-nd-v1">TP-Link TL WR 741 N/ND Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr741n-nd-v2">TP-Link TL WR 741 N/ND Ver:2</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr741n-nd-v4">TP-Link TL WR 741 N/ND Ver:4</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v3">TP-Link TL WR 841 N/ND Ver:3</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v5">TP-Link TL WR 841 N/ND Ver:5</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v7">TP-Link TL WR 841 N/ND Ver:7</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v8">TP-Link TL WR 841 N/ND Ver:8</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr841n-nd-v9">TP-Link TL WR 841 N/ND Ver:9</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr842n-nd-v1">TP-Link TL WR 842 N/ND Ver:1</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr842n-nd-v2">TP-Link TL WR 842 N/ND Ver:2</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr941n-nd-v2">TP-Link TL WR 941 N/ND Ver:2</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr941n-nd-v3">TP-Link TL WR 941 N/ND Ver:3</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr941n-nd-v4">TP-Link TL WR 941 N/ND Ver:4</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-tp-link-tl-wr1043n-nd-v1">TP-Link TL WR 1043 N/ND Ver:1</option>
                 <option value="gluon-fffl-<?php echo $versionNumber; ?>-ubiquiti-bullet-m">Ubiquiti Bullet M</option>
                 <option value="gluon-fffl-<?php echo $versionNumber; ?>-ubiquiti-nanostation-m">Ubiquiti Nanostation M</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-ubiquiti-unifi">Ubiquiti Unifi</option>
-                <option value="gluon-fffl-<?php echo $versionNumber; ?>-ubiquiti-unifiap-outdoor">Ubiquiti Unifiap-Outdoor</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-ubiquiti-unifi">Ubiquiti UniFi</option>
+                <option value="gluon-fffl-<?php echo $versionNumber; ?>-ubiquiti-unifiap-outdoor">Ubiquiti UniFi AP-Outdoor</option>
             </select>
             <button class="button">Herunterladen</button>
             <a class="button" href="files/all_images.zip">Alle als ZIP herunterladen</a>
         </form>
         
-        <p>Software unter <a href="http://de.wikipedia.org/wiki/GNU_General_Public_License">GPL Lizenz</a></p>
+        <p>Die Freifunk Flensburg Firmware steht unter der <a href="http://de.wikipedia.org/wiki/GNU_General_Public_License">GPL Lizenz</a>.</p><br>
+        <p>Du darfst sie somit kostenlos nutzen, studieren, ändern und verbreiten.</p>
         <br>
         <br>
         <script src="http://code.jquery.com/jquery-1.11.0.min.js" type="text/javascript"></script>
